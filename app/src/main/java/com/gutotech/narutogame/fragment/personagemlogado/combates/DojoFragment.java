a30e2da8e0749d27c59e8d4506e09f50f8680d1e
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-03 13:46:39 -0300

    Fix dojo bug

--- app/src/main/java/com/gutotech/narutogame/fragment/personagemlogado/combates/DojoFragment.java
+++ app/src/main/java/com/gutotech/narutogame/fragment/personagemlogado/combates/DojoFragment.java
@@ -1,9 +1,13 @@
 package com.gutotech.narutogame.fragment.personagemlogado.combates;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,20 +26,31 @@ public class DojoFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dojo, container, false);
 
-        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
-                getActivity().getSupportFragmentManager(), FragmentPagerItems.with(getActivity())
-                .add("Batalhas do Dojo NPC", BatalhasDojoNPCFragment.class)
-                .add("Batalhas do Dojo PVP", BatalhasDojoPVPFragment.class)
-                .create()
-        );
+        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_batalhas_dojo_npc:
+                        changeToFragment(new BatalhasDojoNPCFragment());
+                        break;
+                    case R.id.action_batalhas_dojo_pvp:
+                        changeToFragment(new BatalhasDojoPVPFragment());
+                        break;
+                }
+                return true;
+            }
+        });
 
-        ViewPager viewPager = view.findViewById(R.id.viewpager);
-        viewPager.setAdapter(adapter);
-
-        SmartTabLayout smartTabLayout = view.findViewById(R.id.smarttablayout);
-        smartTabLayout.setViewPager(viewPager);
+        changeToFragment(new BatalhasDojoNPCFragment());
 
         return view;
+    }
+
+    private void changeToFragment(Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameConteiner, fragment);
+        transaction.commit();
     }
 
 }
