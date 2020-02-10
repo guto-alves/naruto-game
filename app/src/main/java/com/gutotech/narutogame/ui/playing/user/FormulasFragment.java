82af85632400309b850a4d12f71b98ceb3b5c4af
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2020-02-09 21:47:10 -0300

    Create game formulas UI

--- app/src/main/java/com/gutotech/narutogame/ui/playing/user/FormulasFragment.java
+++ app/src/main/java/com/gutotech/narutogame/ui/playing/user/FormulasFragment.java
@@ -2,26 +2,38 @@ package com.gutotech.narutogame.ui.playing.user;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gutotech.narutogame.R;
+import com.gutotech.narutogame.databinding.FragmentFormulasBinding;
 import com.gutotech.narutogame.ui.SectionFragment;
 import com.gutotech.narutogame.utils.FragmentUtil;
+import com.gutotech.narutogame.utils.StorageUtil;
 
 public class FormulasFragment extends Fragment implements SectionFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root=  inflater.inflate(R.layout.fragment_formulas, container, false);
+        FragmentFormulasBinding binding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_formulas, container, false);
+
+        StorageUtil.downloadProfileForMsg(getActivity(), binding.msgLayout.profileImageView);
+        binding.msgLayout.titleTextView.setText(R.string.about_formulas);
+        binding.msgLayout.descriptionTextView.setText(R.string.about_formulas_description);
 
         FragmentUtil.setSectionTitle(getActivity(), R.string.section_formulas);
 
-        return root;
+        return binding.getRoot();
     }
 
     @Override
